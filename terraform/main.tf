93ec60561dbeafd049c873dba8bd2611536cc5b9
Poojithadevops <gennepoojitha@gmail.com>
2025-03-01 10:26:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_server" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI (Change as per your region)
+  ami           = "ami-0604f27d956d83a4d" # Amazon Linux 2 AMI (Change as per your region)
   instance_type = "t2.micro"
 
   tags = {
